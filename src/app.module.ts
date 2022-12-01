487e675ec2c5c3c53bc3f1831ccbeb41bed07f54
TiveCS <ahmadfathanhanif@gmail.com>
2022-12-01 21:09:28 +0700

    register validation pipes and ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
