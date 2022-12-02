69902d7710398253f27be958ce7049a768c44873
TiveCS <ahmadfathanhanif@gmail.com>
2022-12-02 20:07:48 +0700

    set ConfigModule to global

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,13 @@ import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, ConfigModule.forRoot(), AuthModule],
+  imports: [
+    PrismaModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
