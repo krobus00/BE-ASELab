6f6cd5d9e38b812222d76b715a6048b67a4e788a
TiveCS <ahmadfathanhanif@gmail.com>
2022-12-02 11:12:55 +0700

    init AuthModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, ConfigModule.forRoot()],
+  imports: [PrismaModule, ConfigModule.forRoot(), AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
