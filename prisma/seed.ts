d67b48cc7787f891b087f1cff93c8757631ff011
TiveCS <ahmadfathanhanif@gmail.com>
2022-12-05 12:38:14 +0700

    add db seeding

---
+++ prisma/seed.ts
@@ -0,0 +1,28 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.role.createMany({
+    data: [
+      {
+        name: 'Guest',
+      },
+      {
+        name: 'Admin',
+      },
+      {
+        name: 'SuperAdmin',
+      },
+    ],
+  });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+  });
