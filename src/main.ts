c0e14acb405c7c3d118c24b0b1defa23682514dd
TiveCS <ahmadfathanhanif@gmail.com>
2022-12-05 15:27:06 +0700

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -14,6 +15,13 @@ async function bootstrap() {
   app.enableCors({
     origin: '*',
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('ASELab Backend')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(8080);
 }
