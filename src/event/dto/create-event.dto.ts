a37a5b0138561eaeb9d06cafd001108471f21a5f
TiveCS <ahmadfathanhanif@gmail.com>
2022-12-05 15:54:49 +0700

    add swagger ApiProperty

--- src/event/dto/create-event.dto.ts
+++ src/event/dto/create-event.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import {
   ArrayNotEmpty,
   ArrayUnique,
@@ -8,21 +9,36 @@ import {
 } from 'class-validator';
 
 export class CreateEventDto {
+  @ApiProperty({
+    example: 'ASELab Open Recruitment 2022',
+  })
   @IsString()
   title: string;
 
+  @ApiProperty({
+    example: ['Recruitment'],
+  })
   @IsArray()
   @ArrayNotEmpty()
   @ArrayUnique()
   tags: string[];
 
+  @ApiPropertyOptional({
+    example: 'Telah dibuka open recruitment anggota baru ASELab 2022',
+  })
   @IsString()
   @IsOptional()
   description?: string;
 
+  @ApiProperty({
+    example: '2022-05-30',
+  })
   @IsDateString()
   start_date: string;
 
+  @ApiProperty({
+    example: '2022-06-27',
+  })
   @IsDateString()
   end_date: string;
 }
