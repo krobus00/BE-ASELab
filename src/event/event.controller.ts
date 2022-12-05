b2b55bd08eaed77d59e585c5bd415c0a7fa84698
TiveCS <ahmadfathanhanif@gmail.com>
2022-12-05 15:54:58 +0700

    add jwt guard

--- src/event/event.controller.ts
+++ src/event/event.controller.ts
@@ -5,17 +5,23 @@ import {
   Delete,
   Get,
   Param,
+  ParseIntPipe,
   Post,
   Query,
+  UseGuards,
 } from '@nestjs/common';
 import { GetUserId } from 'src/auth/decorators';
 import { CreateEventDto } from './dto';
+import { JwtGuard } from 'src/auth/guards';
+import { ApiBearerAuth } from '@nestjs/swagger';
 
 @Controller('event')
 export class EventController {
   constructor(private readonly eventService: EventService) {}
 
   @Post()
+  @UseGuards(JwtGuard)
+  @ApiBearerAuth()
   async createEventPost(
     @GetUserId() userId: string,
     @Body() dto: CreateEventDto,
@@ -24,12 +30,14 @@ export class EventController {
   }
 
   @Get()
-  async getAllEvents(@Query('limit') limit: number) {
+  @UseGuards(JwtGuard)
+  async getAllEvents(@Query('limit', ParseIntPipe) limit: number) {
     return this.eventService.getAllEvents(limit);
   }
 
   @Delete('/:id')
-  async deleteEvent(@Param('id') id: number) {
+  @UseGuards(JwtGuard)
+  async deleteEvent(@Param('id', ParseIntPipe) id: number) {
     return this.eventService.deleteEventById(id);
   }
 }
