727f5747e48d016946f25c989005b362fa9527d8
Ahmad Fathan Hanif <41243950+TiveCS@users.noreply.github.com>
2022-12-15 19:07:25 +0700

    Add Member Division (#4)
    * add division table and alter user table
    
    * add MemberModule

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -16,6 +16,47 @@ async function main() {
       },
     ],
   });
+
+  await prisma.division.createMany({
+    data: [
+      {
+        name: 'UI/UX',
+        thumbnail_url: 'https://picsum.photos/200/300',
+      },
+      {
+        name: 'Frontend Engineer',
+        thumbnail_url: 'https://picsum.photos/200/300',
+      },
+      {
+        name: 'Backend Engineer',
+        thumbnail_url: 'https://picsum.photos/200/300',
+      },
+      {
+        name: 'Mobile Engineer',
+        thumbnail_url: 'https://picsum.photos/200/300',
+      },
+      {
+        name: 'Game Programmer',
+        thumbnail_url: 'https://picsum.photos/200/300',
+      },
+      {
+        name: 'Game Designer',
+        thumbnail_url: 'https://picsum.photos/200/300',
+      },
+      {
+        name: 'Audio Composer',
+        thumbnail_url: 'https://picsum.photos/200/300',
+      },
+      {
+        name: 'Game Artist',
+        thumbnail_url: 'https://picsum.photos/200/300',
+      },
+      {
+        name: 'Intern',
+        thumbnail_url: 'https://picsum.photos/200/300',
+      },
+    ],
+  });
 }
 
 main()
